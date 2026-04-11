1601e2e813bf10bf5e80b00cf2907797232c17c0
Pushp Raj <pushp277@gmail.com>
2026-04-11 19:00:21 +0900

    feat(AOP): added demo and definition of Advice and Weaving

---
+++ src/main/java/com/wmware/certification/practice/revision/module2/AOP_demo/advice/AroundAdvice.java
@@ -0,0 +1,4 @@
+package com.wmware.certification.practice.revision.module2.AOP_demo.advice;
+
+public class AroundAdvice {
+}
---
+++ src/main/java/com/wmware/certification/practice/revision/module2/AOP_demo/joinPoints/join2/Join2.java
@@ -0,0 +1,4 @@
+package com.wmware.certification.practice.revision.module2.AOP_demo.joinPoints.join2;
+
+public class Join2 {
+}
